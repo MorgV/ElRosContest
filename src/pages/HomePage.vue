e82dc37432ed9fb5b5663344bec51d14944c56d7
MorgV <129610048+MorgV@users.noreply.github.com>
2024-08-04 21:27:28 +0300

    beta 1

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -10,19 +10,74 @@ const deleteStroke = (id) => {
 const route = () => {
     router.push({ path: '/newOrganization' })
 }
+const filter = (col) => {
+    if (col == 'id') {
+        rootStore.filteredColumn1 = !rootStore.filteredColumn1
+        if (rootStore.filteredColumn1) {
+            rootStore.getOrganization(undefined, undefined, '-id')
+        } else {
+            rootStore.getOrganization(undefined, undefined, 'id')
+        }
+    }
+    if (col == 'name') {
+        rootStore.filteredColumn2 = !rootStore.filteredColumn2
+        if (rootStore.filteredColumn2) {
+            rootStore.getOrganization(undefined, undefined, '-name')
+        } else {
+            rootStore.getOrganization(undefined, undefined, 'name')
+        }
+    }
+    if (col == 'short_name') {
+        rootStore.filteredColumn3 = !rootStore.filteredColumn3
+        if (rootStore.filteredColumn3) {
+            rootStore.getOrganization(undefined, undefined, '-short_name')
+        } else {
+            rootStore.getOrganization(undefined, undefined, 'short_name')
+        }
+    }
+    if (col == 'description') {
+        rootStore.filteredColumn4 = !rootStore.filteredColumn4
+        if (rootStore.filteredColumn4) {
+            rootStore.getOrganization(undefined, undefined, '-description')
+        } else {
+            rootStore.getOrganization(undefined, undefined, 'description')
+        }
+    }
+    if (col == 'is_active') {
+        rootStore.filteredColumn5 = !rootStore.filteredColumn5
+        if (rootStore.filteredColumn5) {
+            rootStore.getOrganization(undefined, undefined, '-is_active')
+        } else {
+            rootStore.getOrganization(undefined, undefined, 'is_active')
+        }
+    }
+}   
 </script>
 
 <template>
     <div class="wrapper">
-        <div class="button"><button type="button" class="btn btn-success" @click="route">Добавить</button></div>
+        <img class="bage" />
+        <div class="button">
+            <button type="button" class="btn btn-success" @click="route">Добавить</button>
+        </div>
         <table class="table table-hover">
             <thead>
                 <tr>
-                    <th scope="col">id</th>
-                    <th scope="col">Полное название</th>
-                    <th scope="col">Короткое название</th>
-                    <th scope="col">Описание</th>
-                    <th scope="col">Активен</th>
+                    <th @click="filter('id')" scope="col">id
+                        <div :class="{ 'active': rootStore.filteredColumn1 }" class="bage"></div>
+                    </th>
+                    <th @click="filter('name')" scope="col">Полное
+                        название<div :class="{ 'active': rootStore.filteredColumn2 }" class="bage"></div>
+                    </th>
+                    <th @click="filter('short_name')" scope="col">
+                        Короткое название<div :class="{ 'active': rootStore.filteredColumn3 }" class="bage"></div>
+                    </th>
+                    <th @click="filter('description')" scope="col">
+                        Описание<div :class="{ 'active': rootStore.filteredColumn4 }" class="bage"></div>
+                    </th>
+                    <th @click="filter('is_active')" scope="col">
+                        Активен<div :class="{ 'active': rootStore.filteredColumn5 }" class="bage"></div>
+                    </th>
                     <th scope="col">
                     </th>
                 </tr>
@@ -32,10 +87,85 @@ const route = () => {
                     :deleteStroke="deleteStroke" />
             </tbody>
         </table>
+        <div class="pagination">
+            <select id="selector" @change="rootStore.getOrganization(1, $event.target.value)" class="form-select"
+                aria-label="Default select example">
+                <option value="1">1</option>
+                <option selected value="5">5</option>
+                <option value="10">10</option>
+                <option value="15">15</option>
+            </select>
+            <nav aria-label="Page navigation example">
+                <ul class="pagination">
+                    <li v-for="el in rootStore.page_count" :key="el" class="page-item">
+                        <a @click="rootStore.getOrganization(el)" class="page-link" href="#">{{ el }}</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     </div>
 </template>
 
 <style scoped>
+* {
+    margin: 0;
+    padding: 0
+}
+
+th {
+    vertical-align: top;
+    text-align: center;
+    cursor: pointer;
+    padding: 0 100px 0 0;
+    position: relative;
+}
+
+th:hover {
+    background-color: #CFCFCF;
+}
+
+.bage {
+    position: absolute;
+    right: -5px;
+    top: 15px;
+}
+
+.active {
+    content: "";
+    position: absolute;
+    right: 0;
+    top: 0;
+    width: 11px;
+    height: 31px;
+    background-repeat: no-repeat;
+    rotate: 180deg;
+    z-index: 10;
+}
+
+.bage::before {
+    content: "";
+    position: absolute;
+    right: 0;
+    top: 0;
+    width: 21px;
+    height: 21px;
+    background-image: url('../assets/arr.svg');
+    background-repeat: no-repeat;
+    rotate: 90deg;
+    z-index: 10;
+}
+
+.form-select {
+    width: 70px;
+}
+
+.pagination {
+    display: flex;
+    justify-content: space-between;
+    max-height: 40px;
+
+}
+
 .wrapper {
     margin: 5% 10%;
     padding: 40px;
--- src/stores/root.js
+++ src/stores/root.js
@@ -6,22 +6,35 @@ export const useRootStore = defineStore('root', {
     organization: [],
     params:{
       page: 1,
-      page_size: 2,
-      ordering: '-id'
+      page_size: 5,
+      ordering: 'id'
     },
+    page_count:[],
+    filteredColumn1: false,
+    filteredColumn2: false,
+    filteredColumn3: false,
+    filteredColumn4: false,
+    filteredColumn5: false,
   }),
   actions: {
-    async getOrganization(page = 1,page_size = 20,ordering ='-id') {
+    async getOrganization(page = this.params.page,page_size=this.params.page_size,ordering=this.params.ordering) {
 
       this.params.page = page
-      this.params.ordering = ordering
+      this.params.ordering = ordering      
       this.params.page_size = page_size
-
+   
+      console.log(this.params)
       const data = await axios.get(BASE_URL,{
         params:this.params
       })
-
-      this.organization = data?.data?.results   
+      this.organization = data?.data?.results  
+      let pc =Math.ceil( data?.data?.count / this.params.page_size)
+      this.page_count = []
+      while(pc !=0){
+        this.page_count.unshift(pc)
+        pc=pc-1 
+      }
+      console.log(this.page_count)
     },
     async addOrganization(addParams) {
       console.log(addParams.short_name)
